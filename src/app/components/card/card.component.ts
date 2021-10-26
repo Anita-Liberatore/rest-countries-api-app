2aa4bfc1220c7eb883318d59db29c92cfb514143
Anita Liberatore <anitaliberatore@hotmail.com>
2021-10-26 21:05:46 +0200

    start card component with api rest and service

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { CountriesService } from 'src/app/countries.service'
+import { Country } from 'src/app/country';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: CountriesService, private http: HttpClient) { }
+
+  item = ['Population', 'Region', 'Capital']
+
+  countries: Country[] = [];
 
   ngOnInit(): void {
-  }
 
+    this.http.get<any>('https://restcountries.com/v3.1/all').subscribe(data => {
+      data.forEach((i: any) =>
+
+        this.countries.push({
+          name: i.name.common,
+          flagPhoto: i.flags.png
+        })
+
+      )
+        console.log(this.countries)
+
+    })
+  }
 }
---
+++ src/app/countries.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Country } from './country';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+
+  constructor(private http: HttpClient) { }
+
+  localUrl = 'https://restcountries.com/v3.1/all';
+
+  getData() {
+    return this.http.get(this.localUrl);
+  }
+  
+}
